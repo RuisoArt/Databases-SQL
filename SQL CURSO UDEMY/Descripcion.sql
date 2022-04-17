34dee986d8babfe66e0c6e9da8a69d9223970bc4
Luis Felipe Narvaez Gomez <ing.felipenarvaez017@gmail.com>
2022-04-17 18:19:59 -0500

    haciendo el curso de udemy

---
+++ SQL CURSO UDEMY/Descripcion.sql
@@ -0,0 +1,54 @@
+-- DESCRIPCION DE CURSO
+-- .............................................................................
+-- Link
+-- .............................................................................
+--https://www.udemy.com/course/sql-curso-completo-de-sql-aprende-desde-cero/learn/lecture/13191096#overview
+-- .............................................................................
+-- Descripcion General
+-- .............................................................................
+-- Acerca de este curso
+-- Aprende SQL desde cero para saber manejar cualquier base de datos
+
+-- Por cifras
+-- Nivel de habilidad: Todos los niveles
+-- Estudiantes: 9271
+-- Idiomas: Español
+-- Subtítulos: Sí
+-- clases: 86
+-- Vídeo: 3 horas en total
+-- Certificados: Consigue el certificado de Udemy al completar todo el curso
+
+-- Características: Disponible en iOS y Android
+
+-- Descripcion:
+-- Aprenderás a realizar consultas básicas y avanzadas, realizar actualizaciones y modificaciones sobre cualquier base de datos, mediante el lenguaje SQL.
+
+-- Estos conocimientos de SQL sirven para cualquier base de datos del mercado: MySQL, Oracle, DB2, SQL Server, y muchas más.
+
+-- ¡Aprender SQL es una de las formas más rápidas para mejorar tus perspectivas profesionales, ya que es una de las habilidades tecnológicas más demandadas actualmente!
+
+-- ¡En este curso aprenderás rápidamente, mediante ejemplos con explicaciones cortas y sencillas!
+
+-- Lo que aprenderás
+--Usar SQL para hacer cualquier tipo de consulta de una base de datos
+--Escribir consultas SQL complejas para una o varias tablas
+--Aprenderas a crear, modificar y optimizar cualquier base de datos
+--Al finalizar este curso, tendrá el conocimiento para administrar y utilizar mejor cualquier base de datos
+--¿Hay requisitos para realizar el curso?
+--No hay requisitios previos. Solo será necesario disponer de un ordenador si se quiere practicar con los ejemplos explicados en el curso
+--¿Para quién es este curso?
+--Cualquier persona interesada en aprender sobre SQL para el manejo de bases de datos o para empezar con el análisis de datos
+
+--Instructores
+-- Redait Media Ingeniería de software
+
+--REDAIT MEDIA es una empresa de IT especializa en software con personal titulado en Ingeniería en Informática con más de 20 años de experiencia en el desarrollo del software.
+-- Somos expertos en la gestión de bases de datos con SQL, y hemos querido compartir con vosotros este curso completo de SQL.
+--También somos expertos en lenguajes de programación y proximamente crearemos algunos cursos sobre programación.
+
+-- Valoraciones
+
+-- 4.4
+-- Calificación: 4.386113 de 5
+-- Valoración del curso
+
